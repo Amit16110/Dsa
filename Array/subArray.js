48deb97a805729835e83e4e57ca4c392aeba3dd7
Amit <amit16110@gmail.com>
2021-01-11 22:31:17 +0530

    SubArray

---
+++ Array/subArray.js
@@ -0,0 +1,29 @@
+// time complexity O(n^2) and space O(1)
+// Need efficient method
+const subArray = (arr, n, sum) => {
+  let currentSum = 0;
+
+  for (let i = 0; i < n; i++) {
+    currentSum = arr[i];
+
+    for (let j = i + 1; j < n; j++) {
+      if (currentSum == sum) {
+        let p = j - 1;
+        console.log("Sum found between indexes " + i + " and " + p);
+        return 1;
+      }
+      if (currentSum > sum || j == n) {
+        break;
+      }
+      currentSum += arr[j];
+    }
+  }
+  console.log("No subarray found");
+  return 0;
+};
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+let n = arr.length;
+let sum = 15;
+
+subArray(arr, n, sum);
